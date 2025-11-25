3690d4bad6ca5a537f4591c7938e51f34257fa25
Rafael Pereira <orafaeldev@gmail.com>
2025-11-24 21:51:09 -0300

    feat: create a images.js file to export gallery images

---
+++ src/images.js
@@ -0,0 +1,13 @@
+import StarryNightArtist from "../public/starry-night/artist.jpg";
+import StarryNightThumbnail from "../public/starry-night/thumbnail.jpg";
+import StarryNightHeroSmall from "../public/starry-night/hero-small.jpg";
+import StarryNightHeroLarge from "../public/starry-night/hero-large.jpg";
+import StarryNightGallery from "../public/starry-night/gallery.jpg";
+
+export const starryNight = {
+  artist: StarryNightArtist,
+  thumbnail: StarryNightThumbnail,
+  heroSmall: StarryNightHeroSmall,
+  heroLarge: StarryNightHeroLarge,
+  gallery: StarryNightGallery,
+};
